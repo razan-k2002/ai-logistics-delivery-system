c53df8dc7a3519c90d7ad42d603f0a6fcbb2c7ba
razan-k2002 <razankawtharani@outlook.com>
2026-05-22 09:51:20 +0300

    merge main into razan and restore latest features

--- frontend/ai_logistics_app/lib/services/api_service.dart
+++ frontend/ai_logistics_app/lib/services/api_service.dart
@@ -5,7 +5,7 @@ class ApiService {
   // Razan's PC:     http://192.168.0.111:3000
   // Teammate's PC:  http://192.168.10.77:3000
   // Android Emulator: http://10.0.2.2:3000
-  static const String baseUrl = 'http://192.168.137.1:3000';
+  static const String baseUrl = 'http://192.168.0.113:3000';
 
   static String? token;
   static int? userId;
